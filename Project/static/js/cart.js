198df10414c0717d4df2e1de05ec307b82fdb8d7
PolynkoPolina <polinalikeukraine@gmail.com>
2025-12-14 21:05:56 +0200

    added changing cart icon when adding a  product

--- Project/static/js/cart.js
+++ Project/static/js/cart.js
@@ -1,4 +1,8 @@
 let cart = document.getElementById("cart-modal");
+let listIdProduct = document.cookie.split('=')[1];
+let uniqueListProducts = new Set(listIdProduct);
+let cartCircle = document.getElementById('full-cart');
+let cartCircleText = document.getElementById('full-cart').getElementsByTagName('span')[0]
 
 function openCart(){
     if (cart.style.display === 'none'){
@@ -11,5 +15,12 @@ function openCart(){
 
 function closeCart(){
     cart.style.display= 'none';
-}
+};
 
+
+if (listIdProduct){
+    cartCircle.style.display = 'flex';
+    cartCircleText.textContent = uniqueListProducts.size - 1;
+} else{
+    cartCircle.style.display = 'none';
+};
--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 import flask
 import flask_login
+from os.path import abspath, join
 
 from Project.config_page import config_page
+from shop.models import Product
 
 
 @config_page(template_name= 'home.html')
 def render_home():
-    return {}
+    model_list_products = Product.query.all()
+    list_products = []
+    for i in range(3):
+        product = model_list_products[i]
+        list_products.append(product)
+    print(list_products)
+    return {
+        'list_product': model_list_products
+    }
+
     
