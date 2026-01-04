69bac83ea405dfb39188fd4901e95dd6cbf8a30a
Polyana Kvasova <polinalikeukraine@gmail.com>
2026-01-04 17:19:21 +0200

    added size changing and style for auth-regisrt modal page, password eye logic

---
+++ cart/static/js/openCloseCart.js
@@ -0,0 +1,35 @@
+const cart = window.parent.document.getElementById('cart-modal');
+let listIdProduct = document.cookie.split('=')[1];
+let uniqueListProducts = new Set(listIdProduct);
+let cartCircle = window.parent.document.getElementById('full-cart');
+let cartCircleText = window.parent.document.getElementById('full-cart').getElementsByTagName('span')[0]
+let cartButton = window.parent.document.getElementById('cart-button');
+
+cartButton.addEventListener(
+    'click',
+    (event) => {
+        if (cart.style.display == 'none'){
+            cart.style.display = 'flex';
+        } 
+        else{
+            cart.style.display= 'none';
+        }
+    }
+);
+
+
+function closeCart(){
+    cart.style.display = 'none';
+};
+
+
+if (listIdProduct){
+    cartCircle.style.display = 'flex';
+    cartCircleText.textContent = uniqueListProducts.size - 1;
+} else{
+    cartCircle.style.display = 'none';
+};
+
+if(listIdProduct.length <= 0){
+    cart.style.height = '47.6vh';
+}
---
+++ user/static/js/auth-registr.js
@@ -0,0 +1,53 @@
+let authRegistrModal = window.parent.document.getElementById('auth-registr-modal');
+let openButton = window.parent.document.getElementById('auth-registr-button');
+let closeButton = document.getElementById('close-auth-registr');
+let backgroundBlurDiv = window.parent.document.querySelector('.background-blur-div');
+let authLink = document.getElementById('auth-link');
+let regisrtLink = document.getElementById('registr-link');
+let logInLink = document.getElementById('log-in-link');
+let arrayPasswordEyes = document.querySelectorAll(".password-eye")
+
+openButton.addEventListener(
+    'click',
+    (event) =>{
+        if (authRegistrModal.style.display === 'none'){
+            authRegistrModal.style.display = 'flex';
+            backgroundBlurDiv.style.display = 'flex';
+        } 
+        else{
+            authRegistrModal.style.display= 'none';
+            backgroundBlurDiv.style.display = 'none';
+        }
+    }
+);
+
+
+function changeMosalSize(){
+    if(authRegistrModal.style.height === '35rem'){
+        authRegistrModal.style.height = '22rem';
+    } else{
+        authRegistrModal.style.height = '35rem';
+    }
+}
+
+
+function closeAuthRegistr(){
+    authRegistrModal.style.display= 'none';
+    backgroundBlurDiv.style.display = 'none';
+};
+
+for(let i=0; i < arrayPasswordEyes.length; i++){
+    let inputObject = arrayPasswordEyes[i].parentElement.getElementsByTagName('input')[0];
+    arrayPasswordEyes[i].addEventListener(
+        'click',
+        (event)=>{
+            if(inputObject.type !='text'){
+                inputObject.type ='text';
+                arrayPasswordEyes[i].style.backgroundImage = 'url(user/static/icon/eye-show.svg)';
+            } else{
+                inputObject.type ='password';
+                arrayPasswordEyes[i].style.backgroundImage = ' url(user/static/icon/eye-not-show.svg)';
+            }
+        }
+    )
+}
